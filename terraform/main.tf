4ec11d6b401e138a6fd77084941f5939650b203e
Panagiotis Peikidis <pekalicious@gmail.com>
2026-01-31 20:39:54 +0200

    Add ECR repositories for backend and frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,29 @@ variable "aws_region" {
 }
 
 data "aws_caller_identity" "current" {}
+
+resource "aws_ecr_repository" "backend" {
+    name = "predictapool-backend"
+    image_tag_mutability = "IMMUTABLE"
+
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+resource "aws_ecr_repository" "frontend" {
+    name = "predictapool-frontend"
+    image_tag_mutability = "IMMUTABLE"
+
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+output "backend_ecr_url" {
+    value = aws_ecr_repository.backend.repository_url
+}
+
+output "frontend_ecr_url" {
+    value = aws_ecr_repository.frontend.repository_url
+}
